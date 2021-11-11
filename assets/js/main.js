2f2dfbc45624aafbcc9cea639d6f322de30faf42
Ahlem Hdhili <halimahdhili@gmail.com>
2021-11-11 09:06:58 +0100

    add find() function & import main.js into html page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,39 @@
-$('.icon-box-faq').on('click', function() {
-    if ($(this).hasClass('active')) {
-        return $(this)
-            .removeClass('active')
-            .css({
-                border: '1px solid #c6d0e3',
-                boxShadow: 'none',
-            })
-            .find('h4')
-            .css({
-                color: '#113681',
-            });
-    }
+$('.icon-box-faq').on('click', function () {
+  if ($(this).hasClass('active')) {
+    return $(this)
+      .removeClass('active')
+      .css({
+        border: '1px solid #c6d0e3',
+        boxShadow: 'none',
+      })
+      .find('h4')
+      .css({
+        color: '#113681',
+      });
+  }
 
-    $('.selectBox')
-
+  $('.selectBox')
+    .find('.icon-box-faq')
     .removeClass('active')
-        .css({
-            border: '1px solid #c6d0e3',
-            boxShadow: 'none',
-        })
-        .find('h4')
-        .css({
-            color: '#113681',
-        });
+    .css({
+      border: '1px solid #c6d0e3',
+      boxShadow: 'none',
+    })
+    .find('h4')
+    .css({
+      color: '#113681',
+    });
 
-    $(this)
-        .addClass('active')
-        .css({
-            border: '1px solid #dc0017',
-            boxShadow: 'rgb(220 0 23) 0px 0px 1px 1px',
-        })
-        .find('h4')
-        .css({
-            color: '#dc0017',
-        });
+  $(this)
+    .addClass('active')
+    .css({
+      border: '1px solid #dc0017',
+      boxShadow: 'rgb(220 0 23) 0px 0px 1px 1px',
+    })
+    .find('h4')
+    .css({
+      color: '#dc0017',
+    });
 });
+
 jQuery;
