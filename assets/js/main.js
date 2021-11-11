6a6771a9de3cdf9bb9fd423db2ac8603a0c45aaa
halimahdhili <halimahdhili@gmail.com>
2021-11-11 08:37:46 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$('.icon-box-faq').on('click', function() {
+    if ($(this).hasClass('active')) {
+        return $(this)
+            .removeClass('active')
+            .css({
+                border: '1px solid #c6d0e3',
+                boxShadow: 'none',
+            })
+            .find('h4')
+            .css({
+                color: '#113681',
+            });
+    }
+
+    $('.selectBox')
+
+    .removeClass('active')
+        .css({
+            border: '1px solid #c6d0e3',
+            boxShadow: 'none',
+        })
+        .find('h4')
+        .css({
+            color: '#113681',
+        });
+
+    $(this)
+        .addClass('active')
+        .css({
+            border: '1px solid #dc0017',
+            boxShadow: 'rgb(220 0 23) 0px 0px 1px 1px',
+        })
+        .find('h4')
+        .css({
+            color: '#dc0017',
+        });
+});
+jQuery;
